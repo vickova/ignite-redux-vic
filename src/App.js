218cdfc281c7742b46fd2070c524e6a62332654f
Victoria Olumide <olumideronke@gmail.com>
2022-07-26 00:07:49 -0700

    Initial Setup

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, {useState} from "react";
+import {useSelector, useDispatch } from 'react-redux';
+
+function App() {
+  // const counter = useSelector(state => state.counter)
+  // const dispatch = useDispatch();
+  return (
+    <div className="App">
+        <h1>Hello Ignite</h1>
+    </div>
+  );
+}
+
+export default App;
